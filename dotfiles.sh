b4bfe3225af866e005ac4873c3367a3a4eabe4fb
Manel Romero <manel@manelromero.com>
2018-08-13 11:27:46 +0200

    Add dotfiles.sh file

---
+++ dotfiles.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+DESTINATION_PATH=~/.dotfiles/
+
+cp ~/dotfiles.sh ${DESTINATION_PATH}dotfiles.sh
+cp ~/.bash_profile ${DESTINATION_PATH}.bash_profile
+cp ~/.gemrc ${DESTINATION_PATH}.gemrc
+cp ~/.git-completion.bash ${DESTINATION_PATH}.git-completion.bash
+cp ~/.git-prompt.sh ${DESTINATION_PATH}.git-prompt.sh
+cp ~/.gitconfig ${DESTINATION_PATH}.gitconfig
+cp ~/.gitignore_global ${DESTINATION_PATH}.gitignore_global
+cp ~/.vimrc ${DESTINATION_PATH}.vimrc
+cp -r ~/.vim/colors ${DESTINATION_PATH}.vim/
+cp ~/.zshrc ${DESTINATION_PATH}.zshrc
+cp ~/Library/Application\ Support/Sublime\ Text\ 3/Packages/User/*.sublime-settings ${DESTINATION_PATH}Sublime/User/
+cp ~/Library/Application\ Support/Sublime\ Text\ 3/Packages/User/*.sublime-keymap ${DESTINATION_PATH}Sublime/User/
